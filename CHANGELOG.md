c4c7501d5c74e8da2221dc2f0fa194ec621f74d2
jtimberman <joshua@opscode.com>
2013-01-08 13:53:11 -0700

    release v1.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## v1.1.2:
+
+* [COOK-2033] - only set recursive permissions if mode is used in
+  mercurial resource
+
 ## v1.1.0:
 
 * [COOK-1945] - cleanup provider
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs mercurial"
-version           "1.1.0"
+version           "1.1.2"
 
 recipe "mercurial", "Installs mercurial"
 
