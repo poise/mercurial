7483b175619ca88719a2e5ebcf42e322a497a1d7
jtimberman <joshua@opscode.com>
2013-02-04 21:51:09 -0700

    add initial test kitchen support

---
+++ test/kitchen/cookbooks/mercurial_test/metadata.rb
@@ -0,0 +1,6 @@
+name              "mercurial"
+maintainer        "Opscode, Inc."
+maintainer_email  "cookbooks@opscode.com"
+license           "Apache 2.0"
+description       "This cookbook is used with test-kitchen to test the parent, mercurial cookbok"
+version           "1.0.0"
