481b708b1e2972667deac01c6007834f7d5e3bf7
Guilhem Lettron <guilhem.lettron@optiflows.com>
2013-11-03 23:42:50 -0500

    [COOK-3794] - Properly manage update for mercurial
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- providers/default.rb
+++ providers/default.rb
@@ -1,59 +1,109 @@
 use_inline_resources
 
+# Support whyrun
+def whyrun_supported?
+  true
+end
+
 action :sync do
-  ::File.delete(hgup_file) if ::File.exist?(hgup_file)
-  execute "sync repository #{new_resource.path}" do
-    not_if "hg identify #{new_resource.path}"
-    command "hg clone --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository} #{new_resource.path} && touch #{hgup_file}"
-    creates hgup_file
-    notifies :run, "execute[set ownership #{new_resource.path}]"
-    notifies :run, "execute[set permissions #{new_resource.path}]"
-  end
-  execute "check incoming changes #{new_resource.path}" do
-    command "hg incoming --rev #{new_resource.reference} #{hg_connection_command}  #{new_resource.repository} && touch #{hgup_file} || true"
-    cwd new_resource.path
-    creates hgup_file
-    notifies :run, "execute[pull #{new_resource.path}]"
-  end
-  execute "pull #{new_resource.path}" do
-    command "hg pull --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository}"
-    cwd new_resource.path
-    only_if { ::File.exist?(hgup_file) }
-    action :nothing
-    notifies :run, "execute[update #{new_resource.path}]"
-  end
-  execute "update #{new_resource.path}" do
-    command "hg update"
-    cwd new_resource.path
-    action :nothing
-    notifies :run, "execute[set ownership #{new_resource.path}]"
-    notifies :run, "execute[set permissions #{new_resource.path}]"
-  end
-  execute "set ownership #{new_resource.path}" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
-    action :nothing
-  end
-  execute "set permissions #{new_resource.path}" do
-    command "chmod -R #{new_resource.mode} #{new_resource.path}"
-    only_if { new_resource.mode }
+  if current_resource.synced
+    Chef::Log.info "#{ new_resource } already synced - nothing to do."
+  elsif current_resource.exists
+    converge_by("Sync #{ @new_resource }") do
+      sync
+    end
+  else
+    converge_by("Clone #{ @new_resource }") do
+      clone
+    end
   end
 end
 
 action :clone do
-  ::File.delete(hgup_file) if ::File.exist?(hgup_file)
-  execute "clone repository #{new_resource.path}" do
-    command "hg clone --rev #{new_resource.reference} #{hg_connection_command} #{new_resource.repository} #{new_resource.path} && touch #{hgup_file}"
-    not_if "hg identify #{new_resource.path}"
-    creates hgup_file
-    notifies :run, "execute[set permission #{new_resource.path}]"
-    notifies :run, "execute[set ownership #{new_resource.path}]"
+  if current_resource.exists
+    Chef::Log.info "#{ new_resource } already exists - nothing to do."
+  else
+    converge_by("Clone #{ @new_resource }") do
+      clone
+    end
   end
-  execute "set ownership #{new_resource.path}" do
-    command "chown -R #{new_resource.owner}:#{new_resource.group} #{new_resource.path}"
+end
+
+def clone
+  execute "clone repository #{new_resource.path}" do
+    command "hg clone #{hg_connection_command} #{new_resource.repository} #{new_resource.path}"
+    user new_resource.owner
+    group new_resource.group
+  end
+  update
+end
+
+def sync
+  execute "pull #{new_resource.path}" do
+    command "hg unbundle -u #{bundle_file}"
+    user new_resource.owner
+    group new_resource.group
+    cwd new_resource.path
+    only_if { ::File.exists?(bundle_file) || repo_incoming? }
+    notifies :delete, "file[#{bundle_file}]"
+  end
+  update
+
+  file bundle_file do
     action :nothing
   end
-  execute "set permission #{new_resource.path}" do
-    command "chmod -R #{new_resource.mode} #{new_resource.path}"
-    only_if { new_resource.mode }
+end
+
+def update
+  execute "hg update for #{new_resource.path}" do
+    command "hg update --rev #{new_resource.reference}"
+    user new_resource.owner
+    group new_resource.group
+    cwd new_resource.path
   end
+end
+
+def load_current_resource
+  init
+  @current_resource = Chef::Resource::Mercurial.new(@new_resource.name)
+  @current_resource.name(@new_resource.name)
+  @current_resource.path(@new_resource.path)
+  if repo_exists?
+    @current_resource.exists = true
+    @current_resource.synced = !repo_incoming?
+  end
+end
+
+def repo_exists?
+  command = Mixlib::ShellOut.new("hg identify #{new_resource.path}").run_command
+  Chef::Log.debug "'hg identify #{new_resource.path}' return #{command.stdout}"
+  return command.exitstatus == 0
+end
+
+def repo_incoming?
+  cmd = "hg incoming --rev #{new_resource.reference} #{hg_connection_command} --bundle #{bundle_file} #{new_resource.repository}"
+  command = Mixlib::ShellOut.new(cmd, :cwd => new_resource.path, :user => new_resource.owner, :group => new_resource.group).run_command
+  Chef::Log.debug "#{cmd} return #{command.stdout}"
+  return command.exitstatus == 0
+end
+
+def init
+  directory tmp_directory do
+    owner new_resource.owner
+    group new_resource.group
+    recursive true
+    mode 0755
+  end
+end
+
+def tmp_directory
+  ::File.join(Chef::Config[:file_cache_path], "mercurial", sanitize_filename(new_resource.path))
+end
+
+def bundle_file
+  ::File.join(tmp_directory, "bundle")
+end
+
+def sanitize_filename(filename)
+  filename.gsub(/[^0-9A-z.\-]/, '_')
 end
